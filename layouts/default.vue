1c9d5166b8a1617bbc234e0debd632c45a068f84
Claudio Bonfati <claudio_bonfati@outlook.com>
2021-06-03 16:47:47 -0300

    Setup default layout file

--- layouts/default.vue
+++ layouts/default.vue
@@ -13,3 +13,11 @@
     mounted() {},
   }
 </script>
+
+<style lang="sass">
+  #page
+    width: 100%
+    height: 100%
+    background: rgb(27,33,56)
+    background: linear-gradient(180deg, rgba(27,33,56,1) 0%, rgba(20,23,38,1) 100%)
+</style>
