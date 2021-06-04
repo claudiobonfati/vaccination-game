70a8af1b4e438090b88b8f85f0b15e7e3d361a23
Claudio Bonfati <claudio_bonfati@outlook.com>
2021-06-04 20:48:55 -0300

    Create HTML structure for Game component

--- components/Game.vue
+++ components/Game.vue
@@ -1,6 +1,95 @@
 <template lang="html">
   <div class="c-game shadow-lg">
-    GAME GOES HERE =)
+    <div id="game-header" class="px-3 px-sm-4 px-md-5 py-3 py-md-4">
+      <div class="row justify-content-center align-items-center no-gutters">
+        <div class="col-6 pr-0 text-left">
+          <h4 class="game-font-timer font-weight-normal d-flex align-items-center">
+            <span class="game-clock mr-1" :class="'gm-'+status"><span></span></span>
+            <span>
+              30:00s
+            </span>
+          </h4>
+          <button id="game-btn-pause">
+            Pause
+          </button>
+          <button id="game-btn-sound">
+            Sound
+          </button>
+        </div>
+        <div class="col-6 pl-3 box-scoreboard">
+          <p class="mb-1 small d-flex align-items-center">
+            <img class="icon-face mr-1" src="~/assets/images/game-face-imune.svg" alt="Vaccinated">
+            <span class="game-border-bottom">Vaccinated:</span>
+            <b class="game-color-yellow w-100 score-percent text-right game-border-bottom">00%</b>
+          </p>
+          <p class="mb-1 small d-flex align-items-center">
+            <img class="icon-face mr-1" src="~/assets/images/game-face-healthy.svg" alt="Healthy">
+            <span class="game-border-bottom">Healthy:</span>
+            <b class="game-color-blue w-100 score-percent text-right game-border-bottom">00%</b>
+          </p>
+          <p class="mb-0 small d-flex align-items-center">
+            <img class="icon-face mr-1" src="~/assets/images/game-face-sick.svg" alt="Infected">
+            <span class="game-border-bottom">Infected:</span>
+            <b class="game-color-red w-100 score-percent text-right game-border-bottom">00%</b>
+          </p>
+        </div>
+      </div>
+    </div>
+    <div class="game-content" :style="{width: this.width + 'px', height: this.height + 'px'}">
+      <div id="game-overlay-initial" class="w-100 h-100" :style="{ backgroundImage: 'url(' + require('@/assets/images/game-background.png') + ')' }" ref="refOverlayInitial">
+        <div class="row no-gutters w-100 h-100 align-items-center justify-content-center">
+          <div class="col-md-6 col-8 pb-5 text-center">
+            <button class="btn btn-default-blue lay-color-black mb-3 w-100">
+              Play
+            </button>
+          </div>
+        </div>
+      </div>
+      <div id="game-overlay-pause" class="w-100 h-100" :style="{ backgroundImage: 'url(' + require('@/assets/images/game-background.png') + ')' }" ref="refOverlayPause">
+        <div class="row no-gutters w-100 h-100 align-items-center justify-content-center">
+          <div class="col-md-7 col-8 pb-5 text-center">
+            <button class="btn btn-default-blue lay-color-black mb-3 w-100">
+              Continue
+            </button>
+            <br>
+            <button class="btn btn-default-orange w-100">
+              Restart
+            </button>
+          </div>
+        </div>
+      </div>
+      <div id="game-overlay-end" class="w-100 h-100 text-left" ref="refOverlayEnd">
+        <div class="row no-gutters w-100 h-100 align-items-center justify-content-center">
+          <section class="col-md-7 col-8 pb-5 text-center">
+            <h5 class="mb-2 h3 font-weight-bold">
+              WIN TITLE HERE
+            </h5>
+            <p class="mb-3">Win description here</p>
+            <br>
+            <button class="btn btn-default-blue w-100 mb-3">
+              Play again
+            </button>
+            <a class="btn btn-default-orange w-100" href="https://youtu.be/oBSandHijDc?t=6" target="_blank">
+              Learn more <feather type="external-link" class="icon"></feather>
+            </a>
+          </section>
+          <section class="col-md-7 col-8 pb-5 text-center">
+            <h5 class="mb-2 d-flex align-items-center h3 font-weight-bold">
+              LOSE TITLE HERE
+            </h5>
+            <p class="mb-3">Lose description here</p>
+            <br>
+            <button class="btn btn-default-blue w-100 mb-3">
+              Try again
+            </button>
+            <a class="btn btn-default-orange w-100" href="https://youtu.be/oBSandHijDc?t=6" target="_blank">
+              Learn more <feather type="external-link" class="icon"></feather>
+            </a>
+          </section>
+        </div>
+      </div>
+      <canvas id="c-game-canvas" ref="refCanvas"></canvas>
+    </div>
   </div>
 </template>
 
@@ -19,12 +108,209 @@
 </script>
 
 <style lang="sass">
+  $color-light-gray: 	#b2b2b2
+  $color-orange: 			#ea9b1c
+
   .c-game
     background-color: #0f111b
     width: 500px
     touch-action: manipulation
     display: block
     position: relative
+    .icon
+      width: 18px
+      height: 18px
+      vertical-align: -3px
+    [class*="btn-default"]
+      display: inline-block
+      padding: 8px 10px
+      border-radius: 5px
+      background: rgb(214,130,104)
+      background: -moz-linear-gradient(90deg, rgba(214,130,104,1) 0%, rgba(223,97,57,1) 100%)
+      background: -webkit-linear-gradient(90deg, rgba(214,130,104,1) 0%, rgba(223,97,57,1) 100%)
+      background: linear-gradient(90deg, rgba(214,130,104,1) 0%, rgba(223,97,57,1) 100%)
+      color: white
+      font-weight: bold
+      border: 0
+      transform: scale(1)
+      transition: transform .2s ease-out
+      box-shadow: 0
+      text-decoration: none
+      &:hover
+        transform: scale(1.05)
+        text-decoration: none
+      &:focus
+        text-decoration: none
+        border: 0
+        box-shadow: 0
+        outline: none
+      &[class*="btn-default-orange"]
+        background: -moz-linear-gradient(90deg, rgba(214,130,104,1) 0%, rgba(223,97,57,1) 100%)
+        background: -webkit-linear-gradient(90deg, rgba(214,130,104,1) 0%, rgba(223,97,57,1) 100%)
+        background: linear-gradient(90deg, rgba(214,130,104,1) 0%, rgba(223,97,57,1) 100%)
+      &[class*="btn-default-blue"]
+        background: rgb(91,173,224);
+        background: -moz-linear-gradient(90deg, rgba(91,173,224,1) 0%, rgba(140,248,198,1) 100%)
+        background: -webkit-linear-gradient(90deg, rgba(91,173,224,1) 0%, rgba(140,248,198,1) 100%)
+        background: linear-gradient(90deg, rgba(91,173,224,1) 0%, rgba(140,248,198,1) 100%)
+        color: #030710
+    .game-font-timer
+      font-size: 2.5rem
+    .game-color-yellow
+      color: #ea9b1c
+    .game-color-blue
+      color: #43B7F2
+    .game-color-red
+      color: #e36037
+    .game-border-bottom
+      border-bottom: 1px solid rgba($color-light-gray, .1)
+    .game-clock
+      display: inline-block
+      position: relative
+      width: 30px
+      height: 30px
+      border-radius: 50%
+      border: 2px solid $color-orange
+      &::before
+        display: block
+        content: " "
+        width: 2px
+        height: calc(40%)
+        background-color: $color-orange
+        position: absolute
+        left: 50%
+        margin-left: -1px
+        top: 10%
+        transform-origin: bottom center
+        border-radius: 2px
+        animation: rotating 2s linear infinite
+        animation-play-state: running
+      &::after
+        display: block
+        content: " "
+        width: 2px
+        height: calc(30%)
+        background-color: $color-orange
+        position: absolute
+        left: 50%
+        margin-left: -1px
+        top: 20%
+        transform-origin: bottom center
+        border-radius: 2px
+        animation: rotating 6s linear infinite
+        animation-play-state: running
+      &.gm-paused,
+      &.gm-not-started,
+      &.gm-ended
+        &::before,
+        &::after
+          animation-play-state: paused
+      span
+        display: block
+        position: absolute
+        width: 4px
+        height: 4px
+        border-radius: 50%
+        background-color: $color-orange
+        top: 50%
+        left: 50%
+        margin-top: -2px
+        margin-left: -2px
+    .game-content
+      position: relative
+      background-color: #030710
+      line-height: 25px
+      overflow: hidden
+    .box-scoreboard 100 h-100p
+    .icon-face
+      width: 25px
+      height: 25px
+    .icon-face-md
+      width: 35px
+      height: 35px
+    [id*="game-overlay"]
+      position: absolute
+      z-index: 2
+      background-repeat: repeat
+      background-position: 0 0
+      background-size: auto 100%
+      animation: animatedBackground 250s linear infinite
+      display: none
+      opacity: 0
+    #c-game-canvas
+      position: absolute
+      z-index: 1
+    [id*="game-btn-"]
+      display: inline-block
+      background-color: transparent
+      border: 0
+      color: $color-light-gray
+      position: relative
+      padding: 0px 13px
+      outline: 0
+      &:focus,
+      &:active
+        outline: 0
+      &.disabled
+        opacity: .2
+      &::before,
+      &::after
+        display: block
+        content: " "
+        position: absolute
+        background-color: $color-light-gray
+      &#game-btn-pause
+        &::before,
+        &::after
+          width: 3px
+          height: 12px
+          top: 50%
+          margin-top: -6px
+          transition: all 0.2s ease-out
+          opacity: 1
+        &::before
+          left: 0
+        &::after
+          left: 5px
+      &#game-btn-sound
+        &::before
+          width: 3px
+          height: 8px
+          top: 50%
+          margin-top: -4px
+        &::before
+          left: 0
+        &::after
+          transition: all 0.2s ease-out
+          top: 50%
+          margin-top: -6px
+          left: 0
+          background-color: transparent
+          width: 0
+          height: 0
+          border-top: 6px solid transparent
+          border-right: 10px solid $color-light-gray
+          border-bottom: 6px solid transparent
+
+  @keyframes animatedBackground
+    from
+      background-position: 0 0
+    to
+      background-position: -1000px 0
+
+  @keyframes rotating
+    from
+      -ms-transform: rotate(0deg)
+      -moz-transform: rotate(0deg)
+      -webkit-transform: rotate(0deg)
+      -o-transform: rotate(0deg)
+      transform: rotate(0deg)
+    to
+      -ms-transform: rotate(360deg)
+      -moz-transform: rotate(360deg)
+      -webkit-transform: rotate(360deg)
+      -o-transform: rotate(360deg)
+      transform: rotate(360deg)
 
   @include media-breakpoint-down(lg)
 
@@ -33,5 +319,7 @@
   @include media-breakpoint-down(sm)
 
   @include media-breakpoint-down(sm)
-
+    .c-game
+      .game-font-timer
+        font-size: 2rem
 </style>
