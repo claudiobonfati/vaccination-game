d7b7cec8dc7afea10941f1ae4ef331b076bdfed4
Claudio Bonfati <claudio_bonfati@outlook.com>
2021-06-06 18:51:11 -0300

    Insert base url settings for Github Pages deploy

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,6 +11,9 @@ export default {
   env: {
     baseUrl: process.env.BASE_URL || 'http://localhost:3000',
   },
+  router: {
+    base: process.env.SUBFOLDER || '',
+  }
   /*
   ** Headers of the page
   */
@@ -34,19 +37,19 @@ export default {
       { property: 'og:locale', content: 'pt_BR' },
     ],
     link: [
-      { rel: 'apple-touch-icon', sizes: '57x57', href: '/img/favicon/apple-icon-57x57.png' },
-      { rel: 'apple-touch-icon', sizes: '60x60', href: '/img/favicon/apple-icon-60x60.png' },
-      { rel: 'apple-touch-icon', sizes: '72x72', href: '/img/favicon/apple-icon-72x72.png' },
-      { rel: 'apple-touch-icon', sizes: '76x76', href: '/img/favicon/apple-icon-76x76.png' },
-      { rel: 'apple-touch-icon', sizes: '114x114', href: '/img/favicon/apple-icon-114x114.png' },
-      { rel: 'apple-touch-icon', sizes: '120x120', href: '/img/favicon/apple-icon-120x120.png' },
-      { rel: 'apple-touch-icon', sizes: '144x144', href: '/img/favicon/apple-icon-144x144.png' },
-      { rel: 'apple-touch-icon', sizes: '152x152', href: '/img/favicon/apple-icon-152x152.png' },
-      { rel: 'apple-touch-icon', sizes: '180x180', href: '/img/favicon/apple-icon-180x180.png' },
-      { rel: 'icon', type: 'image/png', sizes: '192x192', href: '/img/favicon/android-icon-192x192.png' },
-      { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/img/favicon/favicon-32x32.png' },
-      { rel: 'icon', type: 'image/png', sizes: '96x96', href: '/img/favicon/favicon-96x96.png' },
-      { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/img/favicon/favicon-16x16.png' },
+      { rel: 'apple-touch-icon', sizes: '57x57', href: `${process.env.BASE_URL}/img/favicon/apple-icon-57x57.png` },
+      { rel: 'apple-touch-icon', sizes: '60x60', href: `${process.env.BASE_URL}/img/favicon/apple-icon-60x60.png` },
+      { rel: 'apple-touch-icon', sizes: '72x72', href: `${process.env.BASE_URL}/img/favicon/apple-icon-72x72.png` },
+      { rel: 'apple-touch-icon', sizes: '76x76', href: `${process.env.BASE_URL}/img/favicon/apple-icon-76x76.png` },
+      { rel: 'apple-touch-icon', sizes: '114x114', href: `${process.env.BASE_URL}/img/favicon/apple-icon-114x114.png` },
+      { rel: 'apple-touch-icon', sizes: '120x120', href: `${process.env.BASE_URL}/img/favicon/apple-icon-120x120.png` },
+      { rel: 'apple-touch-icon', sizes: '144x144', href: `${process.env.BASE_URL}/img/favicon/apple-icon-144x144.png` },
+      { rel: 'apple-touch-icon', sizes: '152x152', href: `${process.env.BASE_URL}/img/favicon/apple-icon-152x152.png` },
+      { rel: 'apple-touch-icon', sizes: '180x180', href: `${process.env.BASE_URL}/img/favicon/apple-icon-180x180.png` },
+      { rel: 'icon', type: 'image/png', sizes: '192x192', href: `${process.env.BASE_URL}/img/favicon/android-icon-192x192.png` },
+      { rel: 'icon', type: 'image/png', sizes: '32x32', href: `${process.env.BASE_URL}/img/favicon/favicon-32x32.png` },
+      { rel: 'icon', type: 'image/png', sizes: '96x96', href: `${process.env.BASE_URL}/img/favicon/favicon-96x96.png` },
+      { rel: 'icon', type: 'image/png', sizes: '16x16', href: `${process.env.BASE_URL}/img/favicon/favicon-16x16.png` },
     ]
   },
   loading: {
