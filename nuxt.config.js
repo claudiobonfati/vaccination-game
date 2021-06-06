0bd1c3f766b9e1bc98df77ce1029d02fc473716d
Claudio Bonfati <claudio_bonfati@outlook.com>
2021-06-06 18:33:32 -0300

    Insert Travis Continuos Integration with GitHub Pages

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,7 +2,7 @@
 export default {
   server: {
     // port: 8000, // default: 3000
-    host: '192.168.0.103' // default: localhost
+    // host: '192.168.0.103' // default: localhost
   },
   mode: 'universal',
   /*
@@ -19,13 +19,13 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1.0' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' },
+      { hid: 'description', name: 'description', content: 'A game that shows minimalistically how to eradicate a disease in a population.' },
       { hid: 'msapplicationtilecolor', name: 'msapplication-TileColor', content: '#0f111b' },
       { hid: 'msapplicationtileimage', name: 'msapplication-TileImage', content: '/favicon/ms-icon-144x144.png' },
       { hid: 'themecolor', name: 'theme-color', content: '#0f111b' },
       { property: 'og:url', content: process.env.BASE_URL || 'http://localhost:3000' },
       { property: 'og:title', content: 'Vaccination Game' },
-      { property: 'og:description', content: process.env.npm_package_description || '' },
+      { property: 'og:description', content: 'A game that shows minimalistically how to eradicate a disease in a population.' },
       { property: 'og:type', content: 'website' },
       { property: 'og:image', content: `${process.env.BASE_URL}/img/vaccination-game-og.png` || 'http://localhost:3000/img/vaccination-game-og.png' },
       { property: 'og:image:width', content: '1080' },
