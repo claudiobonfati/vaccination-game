789e24b81f7f3fdfcd8bbbf17c404e458d88f03b
Claudio Bonfati <claudio_bonfati@outlook.com>
2021-06-06 19:21:34 -0300

    Setup details for CI

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,10 +9,10 @@ export default {
   ** Global Variables
   */
   env: {
-    baseUrl: process.env.BASE_URL || 'http://localhost:3000',
+    baseUrl: process.env.BASE_URL || 'http://localhost:3000/vaccination-game',
   },
   router: {
-    base: process.env.SUBFOLDER || '',
+    base: '/vaccination-game/',
   },
   /*
   ** Headers of the page
@@ -30,26 +30,26 @@ export default {
       { property: 'og:title', content: 'Vaccination Game' },
       { property: 'og:description', content: 'A game that shows minimalistically how to eradicate a disease in a population.' },
       { property: 'og:type', content: 'website' },
-      { property: 'og:image', content: `${process.env.BASE_URL}/img/vaccination-game-og.png` || 'http://localhost:3000/img/vaccination-game-og.png' },
+      { property: 'og:image', content: `${process.env.BASE_URL || ''}/img/vaccination-game-og.png` },
       { property: 'og:image:width', content: '1080' },
       { property: 'og:image:height', content: '1080' },
       { property: 'og:image:type', content: 'image/png' },
       { property: 'og:locale', content: 'pt_BR' },
     ],
     link: [
-      { rel: 'apple-touch-icon', sizes: '57x57', href: `${process.env.BASE_URL}/img/favicon/apple-icon-57x57.png` },
-      { rel: 'apple-touch-icon', sizes: '60x60', href: `${process.env.BASE_URL}/img/favicon/apple-icon-60x60.png` },
-      { rel: 'apple-touch-icon', sizes: '72x72', href: `${process.env.BASE_URL}/img/favicon/apple-icon-72x72.png` },
-      { rel: 'apple-touch-icon', sizes: '76x76', href: `${process.env.BASE_URL}/img/favicon/apple-icon-76x76.png` },
-      { rel: 'apple-touch-icon', sizes: '114x114', href: `${process.env.BASE_URL}/img/favicon/apple-icon-114x114.png` },
-      { rel: 'apple-touch-icon', sizes: '120x120', href: `${process.env.BASE_URL}/img/favicon/apple-icon-120x120.png` },
-      { rel: 'apple-touch-icon', sizes: '144x144', href: `${process.env.BASE_URL}/img/favicon/apple-icon-144x144.png` },
-      { rel: 'apple-touch-icon', sizes: '152x152', href: `${process.env.BASE_URL}/img/favicon/apple-icon-152x152.png` },
-      { rel: 'apple-touch-icon', sizes: '180x180', href: `${process.env.BASE_URL}/img/favicon/apple-icon-180x180.png` },
-      { rel: 'icon', type: 'image/png', sizes: '192x192', href: `${process.env.BASE_URL}/img/favicon/android-icon-192x192.png` },
-      { rel: 'icon', type: 'image/png', sizes: '32x32', href: `${process.env.BASE_URL}/img/favicon/favicon-32x32.png` },
-      { rel: 'icon', type: 'image/png', sizes: '96x96', href: `${process.env.BASE_URL}/img/favicon/favicon-96x96.png` },
-      { rel: 'icon', type: 'image/png', sizes: '16x16', href: `${process.env.BASE_URL}/img/favicon/favicon-16x16.png` },
+      { rel: 'apple-touch-icon', sizes: '57x57', href: `${process.env.BASE_URL || ''}/img/favicon/apple-icon-57x57.png` },
+      { rel: 'apple-touch-icon', sizes: '60x60', href: `${process.env.BASE_URL || ''}/img/favicon/apple-icon-60x60.png` },
+      { rel: 'apple-touch-icon', sizes: '72x72', href: `${process.env.BASE_URL || ''}/img/favicon/apple-icon-72x72.png` },
+      { rel: 'apple-touch-icon', sizes: '76x76', href: `${process.env.BASE_URL || ''}/img/favicon/apple-icon-76x76.png` },
+      { rel: 'apple-touch-icon', sizes: '114x114', href: `${process.env.BASE_URL || ''}/img/favicon/apple-icon-114x114.png` },
+      { rel: 'apple-touch-icon', sizes: '120x120', href: `${process.env.BASE_URL || ''}/img/favicon/apple-icon-120x120.png` },
+      { rel: 'apple-touch-icon', sizes: '144x144', href: `${process.env.BASE_URL || ''}/img/favicon/apple-icon-144x144.png` },
+      { rel: 'apple-touch-icon', sizes: '152x152', href: `${process.env.BASE_URL || ''}/img/favicon/apple-icon-152x152.png` },
+      { rel: 'apple-touch-icon', sizes: '180x180', href: `${process.env.BASE_URL || ''}/img/favicon/apple-icon-180x180.png` },
+      { rel: 'icon', type: 'image/png', sizes: '192x192', href: `${process.env.BASE_URL || ''}/img/favicon/android-icon-192x192.png` },
+      { rel: 'icon', type: 'image/png', sizes: '32x32', href: `${process.env.BASE_URL || ''}/img/favicon/favicon-32x32.png` },
+      { rel: 'icon', type: 'image/png', sizes: '96x96', href: `${process.env.BASE_URL || ''}/img/favicon/favicon-96x96.png` },
+      { rel: 'icon', type: 'image/png', sizes: '16x16', href: `${process.env.BASE_URL || ''}/img/favicon/favicon-16x16.png` },
     ]
   },
   loading: {
